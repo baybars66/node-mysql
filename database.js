7103a15fdc2daa720752e19cbf2c22b8c99c5631
Baybars ÖNAL <bay66bars@gmail.com>
2020-11-25 14:13:32 +0300

    ilk deneme

---
+++ database.js
@@ -0,0 +1,20 @@
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  //host: "http://
+  //host:"88.250.131.163",
+ host: "88.250.131.163",
+  user: "bay66",
+  password: "super66"
+
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+ // con.query("CREATE DATABASE mydb", function (err, result) {
+   // if (err) throw err;
+    //console.log("Database created");
+ // });
+});
