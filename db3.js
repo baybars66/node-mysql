9098c62dfd1e2a3d388b8d27be7da59d200692ee
Baybars ÖNAL <bay66bars@gmail.com>
2020-11-26 08:18:33 +0300

    bb

--- db3.js
+++ db3.js
@@ -7,57 +7,38 @@ var con = mysql.createConnection({
   database: "mydb"
 });
 
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "88.250.131.163",
-  user: "bay66",
-  password: "super66",
-  database: "mydb"
-});
 /*
+
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "INSERT INTO musteriler (isim, adres) VALUES ('BS Slotion', 'Sultanbeyli 37')";
-  con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("1 record inserted");
-  });*/
-
-/*
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = "INSERT INTO gelenevrak (name, address) VALUES ?";
-    var values = [
-      ['John', 'Highway 71'],
-      ['Peter', 'Lowstreet 4'],
-      ['Amy', 'Apple st 652'],
-      ['Hannah', 'Mountain 21'],
-      ['Michael', 'Valley 345'],
-      ['Sandy', 'Ocean blvd 2'],
-      ['Betty', 'Green Grass 1'],
-      ['Richard', 'Sky st 331'],
-      ['Susan', 'One way 98'],
-      ['Vicky', 'Yellow Garden 2'],
-      ['Ben', 'Park Lane 38'],
-      ['William', 'Central st 954'],
-      ['Chuck', 'Main Road 989'],
-      ['Viola', 'Sideway 1633']
-    ];
-    con.query(sql, [values], function (err, result) {
-      if (err) throw err;
-      console.log("Number of records inserted: " + result.affectedRows);
+    //con.query("SELECT * FROM gelenevrak", function (err, result, fields) {
+    con.query("SELECT name, address FROM gelenevrak", function (err, result, fields) {  
+    //  con.query("SELECT name FROM gelenevrak", function (err, result, fields) {    
+       if (err) throw err;
+         console.log(result);
+         //console.log(result[3].name);
     });
-
 */
+/*
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM gelenevrak", function (err, result, fields) {
+    con.query("SELECT * FROM gelenevrak WHERE address = 'Sancaktepe 37'", function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  
+*/
+
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM gelenevrak WHERE name LIKE 'M%'", function (err, result) {
       if (err) throw err;
       console.log(result);
     });
 
 
+
+
 });
+
